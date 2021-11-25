a85bae7d482ac2c27a62176470429a9d38224478
Jose Sierra <jasierra190@misena.edu.co>
2021-11-24 22:37:51 -0500

    [ADD] Logout added

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -19,4 +19,6 @@ urlpatterns = [
 
     path('posts/', posts_views.list_posts, name='feed'),
     path('user/login', users_views.login_view, name='login'),
+    path('user/logout/', users_views.logout_view, name='logout')
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
